b7867ad6da7cbf77b4201dae6777f405deba7c16
Taha Khan <TahaNKhan@users.noreply.github.com>
2016-10-06 10:20:16 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/login', function (req, res, next) {
 router.get('/userhome', function (req, res) {
     if (req.user)
         res.render('chatapp', {
-            title: "TKChatApp",
+            title: "WebChat",
             user: req.user
         })
     else
