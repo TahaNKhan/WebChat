2d38176e822e5eafbb5cdf33c3f23a462846bd1b
Taha Khan <TahaNKhan@users.noreply.github.com>
2017-02-28 20:58:52 -0800

    Update message.model.js

--- models/message.model.js
+++ models/message.model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//mongoose.connect('mongodb://databaseuser:herokupassword@ds031835.mlab.com:31835/heroku_gjm7qs3c');
+
 var messageSchema = new Schema({
     username: String,
     message: String,
