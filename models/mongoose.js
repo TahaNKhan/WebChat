3ec10113a719d9020a28e20a2a8e9d49c0c8d25a
Taha Khan <TahaNKhan@users.noreply.github.com>
2017-02-28 21:01:30 -0800

    Update mongoose.js

--- models/mongoose.js
+++ models/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://databaseuser:herokupassword@ds031835.mlab.com:31835/heroku_gjm7qs3c');
+mongoose.connect(process.env.MONGODB_URL);
 
 Users = require('./user.model');
 messages = require('./message.model');
